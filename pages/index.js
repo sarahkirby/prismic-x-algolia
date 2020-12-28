5a2124f1f832b3cd7bf81705836cda897a4235ca
Sarah Kirby <sarah@neverbland.com>
2020-12-28 14:35:24 +0100

    Fetch and display all product data on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <h1>Home</h1>;
+import ErrorPage from 'next/error';
+
+import RichText from '../src/components/RichText';
+
+import { allProducts } from '../src/utils/prismic';
+
+export default function Products({ products }) {
+  if (!products) {
+    return <ErrorPage statusCode={404} />;
+  }
+
+  return (
+    <>
+      {products?.map(({ name, _meta }) => (
+        <RichText key={_meta.uid} text={name} />
+      ))}
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const products = await allProducts();
+  return {
+    props: {
+      products,
+    },
+  };
 }
